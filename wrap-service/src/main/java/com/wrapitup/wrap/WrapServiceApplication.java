8c1db40df348d4af30dc5b6ea2f791ae2404315f
SneakySolo <adiofficial15@gmail.com>
2026-09-22 12:24:36 +0530

    kafka pipeline impl

--- wrap-service/src/main/java/com/wrapitup/wrap/WrapServiceApplication.java
+++ wrap-service/src/main/java/com/wrapitup/wrap/WrapServiceApplication.java
@@ -16,23 +16,23 @@ public class WrapServiceApplication {
         SpringApplication.run(WrapServiceApplication.class, args);
     }
 
-    /**
-     * Configure RedisTemplate for caching Wrap results.
-     */
-    @Bean
-    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
-        RedisTemplate<String, Object> template = new RedisTemplate<>();
-        template.setConnectionFactory(connectionFactory);
-
-        StringRedisSerializer stringSerializer = new StringRedisSerializer();
-        GenericJackson2JsonRedisSerializer jsonSerializer = new GenericJackson2JsonRedisSerializer();
-
-        template.setKeySerializer(stringSerializer);
-        template.setValueSerializer(jsonSerializer);
-        template.setHashKeySerializer(stringSerializer);
-        template.setHashValueSerializer(jsonSerializer);
-
-        template.afterPropertiesSet();
-        return template;
-    }
+//    /**
+//     * Configure RedisTemplate for caching Wrap results.
+//     */
+//    @Bean
+//    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+//        RedisTemplate<String, Object> template = new RedisTemplate<>();
+//        template.setConnectionFactory(connectionFactory);
+//
+//        StringRedisSerializer stringSerializer = new StringRedisSerializer();
+//        GenericJackson2JsonRedisSerializer jsonSerializer = new GenericJackson2JsonRedisSerializer();
+//
+//        template.setKeySerializer(stringSerializer);
+//        template.setValueSerializer(jsonSerializer);
+//        template.setHashKeySerializer(stringSerializer);
+//        template.setHashValueSerializer(jsonSerializer);
+//
+//        template.afterPropertiesSet();
+//        return template;
+//    }
 }
